708ff065768b16b86e5336c57437c5d4c0feae0f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-02 20:08:44 +0300

    callback functions

--- project 1/app.js
+++ project 1/app.js
@@ -1,5 +1,13 @@
-let logcall = function(){
-  console.log("logcall was called back")
-}
+//This code waits 5 seconds and prints out "Done!" when the 5 seconds are up. Note that this code will not work in the interpreter because it is not designed for handling callbacks.
+var callback = function () {
+  console.log("React Developer Kimaiyo!");
+};
 
-setTimeout(logcall,300);
+setTimeout(callback, 5000);
+
+//It is also possible to define callbacks as anonymous functions, like so:
+
+setTimeout(function () {
+  console.log("Node  Developer Kimaiyo");
+}, 5000);
+//Like regular functions, callbacks can receive arguments and be executed more than once.
