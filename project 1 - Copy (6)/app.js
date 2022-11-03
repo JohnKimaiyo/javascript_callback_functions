594e62f15d22349dcd41566861d7a4e96a2f48ac
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-11-03 10:23:45 +0300

    callback function

---
+++ project 1 - Copy (6)/app.js
@@ -0,0 +1,9 @@
+var callback = function(){
+    console.log("React developer kimaiyo");
+};
+
+setTimeout(callback,5000);
+
+setTimeout(function(){
+    console.log("Node developer kimaiyo")
+},5000);
